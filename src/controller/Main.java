e121970cd0a74384647773687a29d567bdf0f7e5
Cristian Alejandro Morales Lopez <cristianalejandromoraleslopez@gmail.com>
2020-03-18 10:39:23 -0500

    update names

---
+++ src/controller/Main.java
@@ -0,0 +1,22 @@
+package controller;
+
+import model.Hipodromo;
+
+/**
+ * 
+ * @author jorge
+ *
+ */
+public class Main {
+
+	/**
+	 * 
+	 */
+	private static Hipodromo hipodromo;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		hipodromo = new Hipodromo();
+	}
+
+}
